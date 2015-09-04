904b63f73e30c1d4e411d1e2dde6473ca1b843ed
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2015-09-03 19:43:44 -0700

    Added the "add item" functionality.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,59 @@
-function addItem() {
+// Function to add an item to the shopping list
 
+function addItem() {
+	var info = $('#item-value').val();
+	var list = $('.item-list');
+	var item = $('<li><button class="checkbox">&#x2713;</button><span>' + info + '</span><button class="delete">X</button></li>');
+	var itemValue = $('#item-value');
+
+	// Returns false if the user enters nothing or more than 12 characters
+
+	if (info.length === 0 || info.length > 12) {
+		alert('Please enter an item !!');
+		return false;
+	}
+
+	// Adds the items on top of the list
+
+	list.prepend(item);
+	$(itemValue).val('');
 }
+
+
+
+// Function to check off and uncheck the items in the shopping list
 
 function checkItem() {
 	
 }
 
+
+
+// Function to delete an item from the shopping list
+
 function deleteItem() {
 	
 }
 
-function clearAll() {
+
+
+// Function to clear all the items from the shopping list
+
+/*function clearAll() {
 	$(".clear-list").on("click", ".clear-all", function(){
     	$(".item-list").empty();
     	submissions = 0;
 });
 	console.log("Items deleted");
-}
+}*/
 
 $(function() {
-
-    addItem();
+	var add = $('#submit-item');
+    add.on('click', addItem);
+    /*
     checkItem();
     deleteItem();
     clearAll();
+    */
   
     });
