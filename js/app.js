8865b3be95d312949b56466a93680f758c019a0a
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2016-02-08 21:03:44 -0800

    Made some changes in the app.js 1. Removed the variables and used the targetting directly instead. 2. Moved some of the code out of the document ready function to make the code cleaner and efficient.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 // Function to add an item to the shopping list
 
 function addItem() {
-	var info = $('#item-value').val();
-	var list = $('.item-list');
-	var item = $('<li><button class="checkbox">&#x2713;</button><span>' + info + '</span><button class="delete">X</button></li>');
-	var itemValue = $('#item-value');
+    var info = $('#item-value').val();
+    var list = $('.item-list');
+    var item = $('<li><button class="checkbox">&#x2713;</button><span>' + info + '</span><button class="delete">X</button></li>');
+    var itemValue = $('#item-value');
 
-	// Returns false if the user enters nothing or more than 12 characters
+    // Returns false if the user enters nothing or more than 12 characters
 
-	if (info.length === 0 || info.length > 12) {
-		alert('Please enter an item !!');
-		return false;
-	}
+    if (info.length === 0 || info.length > 12) {
+        alert('Please enter an item !!');
+        return false;
+    }
 
-	// Adds the items on top of the list
+    // Adds the items on top of the list
 
-	list.prepend(item);
-	$(itemValue).val('');
+    list.prepend(item);
+    $(itemValue).val('');
 }
 
 
@@ -24,9 +24,9 @@ function addItem() {
 // Function to check off and uncheck the items in the shopping list
 
 function checkItem() {
-	$(this).parent().toggleClass('checked');
-	// Moves checked items to the bottom of the list
-	$('.item-list').find('.checked').appendTo('.item-list');
+    $(this).parent().toggleClass('checked');
+    // Moves checked items to the bottom of the list
+    $('.item-list').find('.checked').appendTo('.item-list');
 }
 
 
@@ -34,7 +34,7 @@ function checkItem() {
 // Function to delete an item from the shopping list
 
 function deleteItem() {
-	$(this).parent().remove();
+    $(this).parent().remove();
 }
 
 
@@ -43,25 +43,22 @@ function deleteItem() {
 
 function clearAll() {
 
-	$('.item-list').children().remove();
-	/*$(document).on('click', '.clear-all', function(){
-    	$('.item-list').empty();
-    	console.log('Items deleted');
-	});*/
+    $('.item-list').children().remove();
+    /*$(document).on('click', '.clear-all', function(){
+        $('.item-list').empty();
+        console.log('Items deleted');
+    });*/
 }
 
-$(function() {
-	var add = $('#submit-item');
-	var list = $('.item-list');
-	var itemValue = $('#item-value');
-
-    add.on('click', addItem);
-    list.on('click','.delete', deleteItem);
-    list.on('click','.checkbox',checkItem);
+$(document).ready(function () {
+    $('#submit-item').on('click', addItem);
     $('.clear-all').on('click', clearAll);
-    itemValue.on('keypress', function(e){
-    	if (e.which == 13) {
-    		addItem();
-    	}
-    });
 });
+
+$(document).on('keypress', function (e) {
+    if (e.which == 13) {
+        addItem();
+    }
+});
+$(document).on('click', '.delete', deleteItem);
+$(document).on('click', '.checkbox', checkItem);
